358f0d6923a6aefebd12414bf18b3af93d81f1fa
yueliu1999 <yueliu19990731@163.com>
2023-07-04 22:20:53 +0800

    update

---
+++ scripts/train_citeseer.sh
@@ -0,0 +1,1 @@
+python main.py --device cpu --dataset citeseer --hid_units 1536 --lr 5e-4 --epoch 200 --wandb True
---
+++ scripts/train_cora.sh
@@ -0,0 +1,1 @@
+python main.py --device cpu --dataset cora --hid_units 512 --lr 1e-3 --epoch 200 --wandb True
